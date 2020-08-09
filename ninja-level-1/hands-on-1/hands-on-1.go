f295ac2a369b7d52eadf2e2f4084f4705d4a6a07
Alexei Dias <alexeidias@gmail.com>
2020-08-09 09:44:02 -0700

    readme.md reformatted

--- ninja-level-1/hands-on-1/hands-on-1.go
+++ ninja-level-1/hands-on-1/hands-on-1.go
@@ -1,7 +1,9 @@
 // Using the short declaration operator, ASSIGN these VALUES to VARIABLES with the IDENTIFIERS “x” and “y” and “z”
+
 // 42
 // “James Bond”
 // true
+
 // Now print the values stored in those variables using 
 // a single print statement
 // multiple print statements
