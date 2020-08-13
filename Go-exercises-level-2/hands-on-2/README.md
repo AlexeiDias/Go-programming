e455c0d689ec28afe98b91ac4d47dc24b81feeb2
Alexei Dias <alexeidias@gmail.com>
2020-08-13 12:58:03 -0700

    exercise 2 done

---
+++ Go-exercises-level-2/hands-on-2/README.md
@@ -0,0 +1,8 @@
+### using the following operators, write expressions and assign their values to variables:
+
+* g ==
+* h <=
+* i >=
+* j !=
+* k <
+* l >
---
+++ Go-exercises-level-2/hands-on-2/hands-on-2.go
@@ -0,0 +1,30 @@
+// using the following operators, write expressions and assign their values to variables:
+
+// g ==
+// h <=
+// i >=
+// j !=
+// k <
+// l >
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	g := 44
+	h := 43
+	i := 44
+	j := 43
+	k := 44
+	l := 43
+	fmt.Println(g == h)
+	fmt.Println(h <= i)
+	fmt.Println(i >= j)
+	fmt.Println(j != k)
+	fmt.Println(k < l)
+	fmt.Println(l > g)
+
+}
