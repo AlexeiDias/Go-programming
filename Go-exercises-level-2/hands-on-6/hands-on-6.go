e9774d1ea7fda15e014f0b9b1dd7b42f55c06e5a
Alexei Dias <alexeidias@gmail.com>
2020-08-13 15:01:49 -0700

    exercise 6 corrected

--- Go-exercises-level-2/hands-on-6/hands-on-6.go
+++ Go-exercises-level-2/hands-on-6/hands-on-6.go
@@ -1,3 +1,27 @@
 //	Using iota, create 4 constant for the last 4 years
 //	Print the constan value
 
+//	Create a variable type spring using a raw string literal.
+//	Print it
+
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a = 2017 + iota
+	b = 2017 + iota
+	c = 2017 + iota
+	d = 2017 + iota
+)
+
+func main() {
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+}
